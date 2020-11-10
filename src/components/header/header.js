713fe26e650f28b0ef3053ee22f076d52d4dd8f7
Bogdan-create <bogdan-shigaev@mail.ru>
2020-11-10 10:15:19 +0600

    add header

---
+++ src/components/header/header.js
@@ -0,0 +1,67 @@
+import search from '../../assets/search.svg';
+import phone from '../../assets/phone.svg';
+import cart from '../../assets/cart.svg';
+import user from '../../assets/user.svg';
+import './styles.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <div className='header__top'>
+          <div>
+            <img className="header__phone" src={phone} alt='phone' />
+            <img src={search} alt='search' />
+          </div>
+          <div className='header__logo'>
+            <a href='' className='logo__link'>
+              Shoply
+            </a>
+          </div>
+          <div>
+            <img className="header__cart" src={cart} alt='cart' />
+            <img src={user} alt='user' />
+          </div>
+        </div>
+        <div className='header__bottom'>
+          <nav className='header__nav nav'>
+            <ul className='nav__menu'>
+              <li className='nav__item'>
+                <a href='#hero' className='nav__link'>
+                  Home
+                </a>
+              </li>
+              <li className='nav__item'>
+                <a href='#ajara' className='nav__link'>
+                  New In
+                </a>
+              </li>
+              <li className='nav__item'>
+                <a href='' className='nav__link'>
+                  Women Product
+                </a>
+              </li>
+              <li className='nav__item'>
+                <a href='' className='nav__link'>
+                  Men Product
+                </a>
+              </li>
+              <li className='nav__item'>
+                <a href='' className='nav__link'>
+                  Top Brands
+                </a>
+              </li>
+              <li className='nav__item'>
+                <a href='' className='nav__link'>
+                  Special Offers
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
